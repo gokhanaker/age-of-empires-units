6129af58632adc1d8609fa4011278f26e7da3535
gokhanaker <gokhanaker@gmail.com>
2025-03-21 16:30:18 +0300

    test: added unit tests for navbar component

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,16 +11,38 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
-    })
-    .compileComponents();
+      imports: [
+        NavbarComponent,
+        MatToolbarModule,
+        MatButtonModule,
+        RouterModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: { paramMap: { get: () => null } },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create navbar component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the toolbar with navigation buttons', () => {
+    const compiled = fixture.nativeElement;
+    const toolbar = compiled.querySelector('mat-toolbar');
+    const navButtons = compiled.querySelectorAll('a');
+
+    expect(toolbar).toBeTruthy();
+    expect(navButtons.length).toBeGreaterThan(0);
   });
 });
