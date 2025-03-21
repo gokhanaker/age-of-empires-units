445395e79d0de8a4e27ec78b7721ec7d3279a0af
gokhanaker <gokhanaker@gmail.com>
2025-03-21 18:11:10 +0300

    test: added unit tests for unit service

--- src/app/services/unit.service.spec.ts
+++ src/app/services/unit.service.spec.ts
@@ -1,16 +1,94 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UnitService } from './unit.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { loadUnitsSuccess, loadUnitsFailure } from '@state/unit.actions';
+import { Age } from '@models/ages.model';
+import { mockUnits } from '../test-helper/test-helper';
 
 describe('UnitService', () => {
   let service: UnitService;
+  let httpMock: HttpTestingController;
+  let store: MockStore;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UnitService, provideMockStore()],
+    });
+
     service = TestBed.inject(UnitService);
+    httpMock = TestBed.inject(HttpTestingController);
+    store = TestBed.inject(MockStore);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should fetch units and dispatch loadUnitsSuccess', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+
+    service.fetchUnits();
+    const req = httpMock.expectOne('assets/data/units.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUnits);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      loadUnitsSuccess({ units: mockUnits })
+    );
+  });
+
+  it('should dispatch loadUnitsFailure on error', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+
+    service.fetchUnits();
+    const req = httpMock.expectOne('assets/data/units.json');
+    expect(req.request.method).toBe('GET');
+    req.flush('Error', { status: 500, statusText: 'Server Error' });
+
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      loadUnitsFailure({
+        error:
+          'Http failure response for assets/data/units.json: 500 Server Error',
+      })
+    );
+  });
+
+  describe('filterUnits', () => {
+    it('should filter units by age', () => {
+      const filteredUnits = service.filterUnits(
+        mockUnits,
+        Age.Feudal,
+        { food: false, wood: false, gold: false },
+        { food: 0, wood: 0, gold: 0 }
+      );
+      expect(filteredUnits.length).toBe(1);
+      expect(filteredUnits[0].name).toBe('Archer');
+    });
+
+    it('should filter units by food cost', () => {
+      const filteredUnits = service.filterUnits(
+        mockUnits,
+        Age.Feudal,
+        { food: true, wood: false, gold: false },
+        { food: 50, wood: 0, gold: 0 }
+      );
+      expect(filteredUnits.length).toBe(1);
+      expect(filteredUnits[0].name).toBe('Archer');
+    });
+
+    it('should return all units if no filters are applied', () => {
+      const filteredUnits = service.filterUnits(
+        mockUnits,
+        Age.All,
+        { food: false, wood: false, gold: false },
+        { food: 0, wood: 0, gold: 0 }
+      );
+      expect(filteredUnits.length).toBe(2);
+    });
   });
 });
